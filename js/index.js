d9c9a5522992b14aa14ae95983b850263f09a46c
Luis Fernando Paucar <lfernandopaucar28@gmail.com>
2024-08-14 18:48:39 -0500

    Inclusion de objetos, arrays y funciones de orden superior

--- js/index.js
+++ js/index.js
@@ -1,88 +1,90 @@
 // Calculadora de cuotas mensuales de creditos no mayores a s/5000.
-let monto = parseFloat(prompt("Introdusca su monto(Recordar que no debe ser mayor a s/5000): "));
-let plazo = parseFloat(prompt("Introdusca la cantidad de meses en las que desea pagar(Maximo 18 meses): "));
 
 
+class Credito{
+    constructor(nombre, monto, plazo){
+        this.nombre = nombre;
+        this.monto = monto;
+        this.plazo = plazo;
+    }
+
+    cuotaPagar(){
+        const tasa = asignarTasaAnual(this.monto);
+        if (tasa) {
+            return (this.monto * tasa * Math.pow(1 + tasa, this.plazo)) / (Math.pow(1 + tasa, this.plazo) - 1);
+        } else {
+            return 0;
+        }
+    };
+    montoTotalApagar(){
+        return this.cuotaPagar() * this.plazo;
+    };
+}
 
 
-function ingresarMonto(monto){
-    if(monto == " "){
-        console.error("No ah introducido nada aun, intente nuevamente")
-    }else if(monto == 0){
-        console.log("Ingrese un monto mayor a 0")
-    }else if((monto > 0)&&(monto <= 5000)){
-        console.log("Su monto igresado es: " + monto)
-    }else {
-        console.log("Su monto ingresado es mayor al permitido")
+// En este apartado se le asignara la tasa correspondiente segun el monto ingresado:
+function asignarTasaAnual(monto){
+    if(monto >= 100 && monto <= 1500){
+        return 0.045/12;
+    } else if(monto >= 1500 && monto <= 3000){
+            return 0.035/12;
+    }else if(monto >= 3000 && monto <= 5000){
+        return 0.025/12;
+    } else{
+        alert("Servicio interrunpido")
+        return null;
     }
 }
 
 
-function plazoIngresado(plazo){
-    if(plazo == " "){
-        console.error("No ah introducido nada aun, intente nuevamente")
-    }else if(plazo == 0){
-        console.log("Este no es un plazo valido")
-    }else if((plazo > 0)&&(plazo <= 18)){
-        console.log("El plazo ingresado es de: " + plazo + " Meses")
-    }else {
-        console.log("El plazo ingresado es superior al permitido")
+
+
+const plazo = (anios, periodo) => anios * frecuencia(periodo);
+
+
+// Aqui se evaluara el plazo que indique el usuario.
+function frecuencia(periodo){
+    if(periodo == "mensual"){
+        return 12;
+    }else if(periodo == "bimestral"){
+        return 6;
+    }else if(periodo == "trimestral"){
+        return 4;
+    }else if(periodo == "semestral"){
+        return 2;
+    }else{
+        return 1; // El periodo por defecto es anual
     }
 }
 
 
+// La cantidad de creditos simuladas se guardaran aqui: 
+const arrayCreditos = []; 
 
-ingresarMonto(monto);
-plazoIngresado(plazo);
+let limiteCreditos = prompt("Colaca la cantidad de creditos que deseas simular(maximo 10 simulaciones): ")
 
+for (let i = 0; i < limiteCreditos && i <= 10; i++){
+    const nombre = prompt("Ingrese su nombre: ");
+    const monto = parseFloat(prompt("Ingrese el monto de su prestamo(minimo $100 y maximo $5000: "));
+    const anios = parseFloat(prompt("Ingrese la cantidad de años: "));
+    const periodo = prompt("Ingrese el periodo de pago preferido(mensual, bimestral, trimestral o semestral): ");
 
-for (let i = 2.5; i <= 4.5; i++){
+    const prestamo = new Credito(nombre, monto, plazo(anios, periodo));
 
-    if(i != 2.5){
-        continue;
+    if(prestamo.cuotaPagar() !== 0) {
+        arrayCreditos.push(prestamo);
+    }else{
+        alert("Simluacion interrunpida")
     }
-    console.log("Taza disponible: " + i)
-
 }
 
-for (let i = 2.5; i <= 4.5; i++){
+const hoy = new Date();
+console.log(hoy.toDateString());
 
-    if(i != 3.5){
-        continue;
-    }
-    console.log("Taza disponible: " + i)
-
-}
-
-for (let i = 2.5; i <= 4.5; i++){
-
-    if(i != 4.5){
-        continue;
-    }
-    console.log("Taza disponible: " + i)
-
-}
-
-let tasaAnual = prompt("Escoge tu taza: (Recordar los tipos de taza para cada monto: montos iguales o menores a s/1500 es 4.5% anual/para montos mayores a s/1500 y menores o iguales a s/3000 es 3.5% anual/ para montos mayores a s/3000 y maximo s/5000 es 2.5% anual")
-
-switch(tasaAnual){
-    case 4.5:
-        console.log("La tasa correspondiente a su monto es 4.5%");
-    case 3.5:
-        console.log("La tasa correspondiente a su monto es 3.5%");
-    case 2.5:
-        console.log("La tasa correspondiente a su monto es 2.5%");
-}
+arrayCreditos.forEach((credito) => { 
+    console.log("La cuota de " + credito.nombre + " a pagar es de: $"+ Math.round(credito.cuotaPagar()) + " mensuales")});
 
 
-const tasaMensual = (tasaAnual / 100) / 12;
 
-const calcularCuota = (a, b, c) => (a * b) / (1 - Math.pow((1 + b), -c))
 
-console.log("Su cuota a pagar mensual es de: " + calcularCuota(monto, tasaMensual, plazo))
 
-let cuotaPagar = calcularCuota(monto, tasaMensual, plazo);
-
-const totalInteres = (a, b, c) => ((c * b) - a);
-
-console.log("El interes total a pagar es de: " + totalInteres(monto, plazo, cuotaPagar))
